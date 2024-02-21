984c4e13c6185c9fa2f25f72f5fcfc2379968a48
gxvl <geraldo.goncalves@ufrpe.br>
2024-02-21 02:55:00 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]); // colocamos esse state aqui no app (pai) para podermos
+  // utilizar ele no logo e no packinglist (filhos) através de props.
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id)); //vamos usar o setItem para mudar o nosso estado.
+    // com isso, fazemos essa callback function para usar o filter e colocar no nosso array novo aqueles itens que não têm o id da nossa prop (o item que queremos deletar)
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure that you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
